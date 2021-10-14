fb4670c82fa13e1fea2925259c903d2e92a0e75c
acagirici <acagirici23@gmail.com>
2021-10-14 19:06:13 -0400

    react-playground

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const[count,setCount] = useState(0)
+  const[data, setData] = useState()
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await fetch("https://swapi.dev/api/people/");
+      const newData = await response.json();
+      setData(newData);
+    };
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {count}
+      </h1>
+      <div className="button-wrapper">
+        <button onClick={()=>setCount(count-1)}>-</button>
+        <button onClick={()=>setCount(count+1)}>+</button>
+        <button onClick={()=>setCount(count*0)}>Reset</button>
+        <div>{newData.map(data => {
+          <li key={data.id}>data.name</li>
+        })}</div>
+      </div>
     </div>
   );
 }
